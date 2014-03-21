4ac3c2864ba485b8ebf4d2fb35857d6a02738009
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-03-21 02:09:48 -0800

    added primary output
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18105528]

--- nvBWT/nvBWT.cu
+++ nvBWT/nvBWT.cu
@@ -403,6 +403,7 @@ int build(
 
         timer.stop();
         log_info(stderr, "building forward BWT... done: %um:%us\n", uint32(timer.seconds()/60), uint32(timer.seconds())%60);
+        log_info(stderr, "  primary: %u\n", primary);
 
         // save it to disk
         {
@@ -456,6 +457,7 @@ int build(
 
         timer.stop();
         log_info(stderr, "building reverse BWT... done: %um:%us\n", uint32(timer.seconds()/60), uint32(timer.seconds())%60);
+        log_info(stderr, "  primary: %u\n", primary);
 
         // save it to disk
         {

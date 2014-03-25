8a90aa50e95f43c83d5e23acb375764d6f9eba37
Nuno Subtil <nsubtil@nvidia.com>
2014-03-25 09:26:41 -0800

    io: Remove spurious proprietary copyright
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18130454]

--- nvbio/io/output/output_debug.cpp
+++ nvbio/io/output/output_debug.cpp
@@ -25,17 +25,6 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
-/*
- * Copyright (c) 2012-13, NVIDIA CORPORATION.  All rights reserved.
- *
- * NVIDIA CORPORATION and its licensors retain all intellectual property
- * and proprietary rights in and to this software, related documentation
- * and any modifications thereto.  Any use, reproduction, disclosure or
- * distribution of this software and related documentation without an express
- * license agreement from NVIDIA CORPORATION is strictly prohibited.
- */
-
 #include <nvbio/io/output/output_debug.h>
 #include <nvbio/io/fmi.h>
 #include <nvbio/basic/numbers.h>

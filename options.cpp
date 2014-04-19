7b6f4dc2b5d457fdb292b40ad590e8b324b45c8a
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-04-18 21:50:42 -0800

    fixed windows build
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/internal/": change = 18311764]

--- options.cpp
+++ options.cpp
@@ -1,14 +1,7 @@
+#include "options.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-#if !defined(WIN32)
-
-#include <unistd.h>
-#include <getopt.h>
-
-#include <nvbio/basic/console.h>
-
-#include "options.h"
+#include <string.h>
 
 struct runtime_options command_line_options;
 
@@ -20,6 +13,35 @@ static void usage(void)
     fprintf(stderr, "\n");
     exit(1);
 }
+
+#if defined(WIN32)
+
+void parse_command_line(int argc, char **argv)
+{
+    for (int i = 0; i < argc-3; ++i)
+    {
+        if (strcmp( argv[i], "-f" ) == 0 ||
+            strcmp( argv[i], "--file-ref" ) == 0)
+            command_line_options.genome_use_mmap = false;
+    }
+
+    if (argc < 3)
+    {
+        // missing required arguments or too many arguments
+        usage();
+    }
+
+    command_line_options.genome_file_name = argv[argc-3];
+    command_line_options.input_file_name  = argv[argc-2];
+    command_line_options.output_file_name = argv[argc-1];
+}
+
+#else
+
+#include <unistd.h>
+#include <getopt.h>
+
+#include <nvbio/basic/console.h>
 
 void parse_command_line(int argc, char **argv)
 {

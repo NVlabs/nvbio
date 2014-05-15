86c5dbd3a28b1eda9a82f0ba7e38ab3ead04d9a0
Nuno Subtil <nsubtil@nvidia.com>
2014-05-14 18:33:15 -0800

    Add 4-bit DNA string conversion functions
    Change-Id: I94c324f9298d5c3b0d5e54df61651037d0e2ff9f
    
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18472265]

--- nvbio/basic/dna.h
+++ nvbio/basic/dna.h
@@ -41,6 +41,29 @@ NVBIO_FORCEINLINE NVBIO_HOST_DEVICE char dna_to_char(const uint8 c)
            c == 3 ? 'T' :
                     'N';
 }
+
+/// convert a 4-bit DNA symbol to its ASCII character
+///
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE char dna16_to_char(const uint8 c)
+{
+    return c ==  0 ? '=' :
+           c ==  1 ? 'A' :
+           c ==  2 ? 'C' :
+           c ==  3 ? 'M' :
+           c ==  4 ? 'G' :
+           c ==  5 ? 'R' :
+           c ==  6 ? 'S' :
+           c ==  7 ? 'V' :
+           c ==  8 ? 'T' :
+           c ==  9 ? 'W' :
+           c == 10 ? 'Y' :
+           c == 11 ? 'H' :
+           c == 12 ? 'K' :
+           c == 13 ? 'D' :
+           c == 14 ? 'B' :
+                     'N';
+}
+
 /// convert an ASCII DNA representation to its 2-bit symbol
 ///
 NVBIO_FORCEINLINE NVBIO_HOST_DEVICE uint8 char_to_dna(const char c)
@@ -50,6 +73,28 @@ NVBIO_FORCEINLINE NVBIO_HOST_DEVICE uint8 char_to_dna(const char c)
            c == 'G' ? 2u :
            c == 'T' ? 3u :
                       4u;
+}
+
+/// convert an ASCII DNA representation to its 4-bit symbol
+///
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE uint8 char_to_dna16(const char c)
+{
+    return c == '=' ?  0u :
+           c == 'A' ?  1u :
+           c == 'C' ?  2u :
+           c == 'M' ?  3u :
+           c == 'G' ?  4u :
+           c == 'R' ?  5u :
+           c == 'S' ?  6u :
+           c == 'V' ?  7u :
+           c == 'T' ?  8u :
+           c == 'W' ?  9u :
+           c == 'Y' ? 10u :
+           c == 'H' ? 11u :
+           c == 'K' ? 12u :
+           c == 'D' ? 13u :
+           c == 'B' ? 14u :
+                      15u;
 }
 
 /// convert a 2-bit DNA string to an ASCII string
@@ -79,6 +124,35 @@ NVBIO_FORCEINLINE NVBIO_HOST_DEVICE void dna_to_string(
 
     string[ end - begin ] = '\0';
 }
+
+/// convert a 2-bit DNA string to an ASCII string
+///
+template <typename SymbolIterator>
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE void dna16_to_string(
+    const SymbolIterator begin,
+    const uint32 n,
+    char* string)
+{
+    for (uint32 i = 0; i < n; ++i)
+        string[i] = dna16_to_char( begin[i] );
+
+    string[n] = '\0';
+}
+
+/// convert a 2-bit DNA string to an ASCII string
+///
+template <typename SymbolIterator>
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE void dna16_to_string(
+    const SymbolIterator begin,
+    const SymbolIterator end,
+    char* string)
+{
+    for (SymbolIterator it = begin; it != end; ++it)
+        string[ (it - begin) % (end - begin) ] = dna16_to_char( *it );
+
+    string[ end - begin ] = '\0';
+}
+
 /// convert an ASCII DNA string to its 2-bit representation
 ///
 template <typename SymbolIterator>
@@ -100,6 +174,29 @@ NVBIO_FORCEINLINE NVBIO_HOST_DEVICE void string_to_dna(
 {
     for (const char* it = begin; *it != '\0'; ++it)
         symbols[ it - begin ] = char_to_dna( *it );
+}
+
+/// convert an ASCII DNA string to its 4-bit representation
+///
+template <typename SymbolIterator>
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE void string_to_dna16(
+    const char* begin,
+    const char* end,
+    SymbolIterator symbols)
+{
+    for (const char* it = begin; it != end; ++it)
+        symbols[ (it - begin) % (end - begin) ] = char_to_dna16( *it );
+}
+
+/// convert a NULL-terminated ASCII DNA string to its 4-bit representation
+///
+template <typename SymbolIterator>
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE void string_to_dna16(
+    const char* begin,
+    SymbolIterator symbols)
+{
+    for (const char* it = begin; *it != '\0'; ++it)
+        symbols[ it - begin ] = char_to_dna16( *it );
 }
 
 } // namespace nvbio

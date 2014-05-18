fba9003b527dc706736ca37045dc5bce85fe42c6
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-05-18 09:28:17 -0800

    fixed method signature
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18484157]

--- nvbio/io/sequence/sequence.h
+++ nvbio/io/sequence/sequence.h
@@ -620,7 +620,7 @@ struct SequenceDataStream
 
     /// next batch
     ///
-    bool next(struct SequenceDataEncoder* encoder, const uint32 batch_size, const uint32 batch_bps = uint32(-1)) {}
+    int next(struct SequenceDataEncoder* encoder, const uint32 batch_size, const uint32 batch_bps = uint32(-1)) {}
 
     /// is the stream ok?
     ///

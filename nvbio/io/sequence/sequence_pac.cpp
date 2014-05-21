fdc8d73aeebf405335b48f75f5ae9485e3ee31a4
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-05-21 09:21:04 -0800

    minor changes
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18496412]

--- nvbio/io/sequence/sequence_pac.cpp
+++ nvbio/io/sequence/sequence_pac.cpp
@@ -134,7 +134,7 @@ bool load_pac(
             }
 
             // build the input wpac stream
-            typedef PackedStream<uint32*,uint8,2,true> pac_stream_type;
+            typedef PackedStream<const uint32*,uint8,2,true> pac_stream_type;
             pac_stream_type pac( pac_stream );
 
             // build the output stream
@@ -177,7 +177,7 @@ bool load_pac(
         }
 
         // build the input pac stream
-        typedef PackedStream<uint8*,uint8,2,true> pac_stream_type;
+        typedef PackedStream<const uint8*,uint8,2,true> pac_stream_type;
         pac_stream_type pac( pac_stream );
 
         // build the output stream
@@ -333,7 +333,7 @@ bool load_pac(
         return load_pac<DNA>( prefix, &sequence_data->m_sequence_vec[0], seq_length, seq_words );
         break;
     case DNA_N:
-        return load_pac<DNA>( prefix, &sequence_data->m_sequence_vec[0], seq_length, seq_words );
+        return load_pac<DNA_N>( prefix, &sequence_data->m_sequence_vec[0], seq_length, seq_words );
         break;
     case PROTEIN:
         return load_pac<PROTEIN>( prefix, &sequence_data->m_sequence_vec[0], seq_length, seq_words );
@@ -458,7 +458,7 @@ bool load_pac(
             return load_pac<DNA>( prefix, sequence_ptr, seq_length, seq_words );
             break;
         case DNA_N:
-            return load_pac<DNA>( prefix, sequence_ptr, seq_length, seq_words );
+            return load_pac<DNA_N>( prefix, sequence_ptr, seq_length, seq_words );
             break;
         case PROTEIN:
             return load_pac<PROTEIN>( prefix, sequence_ptr, seq_length, seq_words );

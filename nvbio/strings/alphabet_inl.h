ad830fea840a3ff5315d1b44f3912d24392adb52
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-05-27 00:17:30 -0800

    added support for the protein alphabet
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18517424]

--- nvbio/strings/alphabet_inl.h
+++ nvbio/strings/alphabet_inl.h
@@ -32,6 +32,66 @@
 
 namespace nvbio {
 
+/// convert a Protein symbol to its ASCII character
+///
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE char protein_to_char(const uint8 c)
+{
+    return c ==  0 ? 'A' :
+           c ==  1 ? 'C' :
+           c ==  2 ? 'D' :
+           c ==  3 ? 'E' :
+           c ==  4 ? 'F' :
+           c ==  5 ? 'G' :
+           c ==  6 ? 'H' :
+           c ==  7 ? 'I' :
+           c ==  8 ? 'K' :
+           c ==  9 ? 'L' :
+           c == 10 ? 'M' :
+           c == 11 ? 'N' :
+           c == 12 ? 'P' :
+           c == 13 ? 'Q' :
+           c == 14 ? 'R' :
+           c == 15 ? 'S' :
+           c == 16 ? 'T' :
+           c == 17 ? 'V' :
+           c == 18 ? 'W' :
+           c == 19 ? 'Y' :
+           c == 20 ? 'B' :
+           c == 21 ? 'Z' :
+           c == 22 ? 'X' :
+                     'N';
+}
+
+/// convert an ASCII character to a Protein symbol
+///
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE uint8 char_to_protein(const char c)
+{
+    return c == 'A' ? 0u  :
+           c == 'C' ? 1u  :
+           c == 'D' ? 2u  :
+           c == 'E' ? 3u  :
+           c == 'F' ? 4u  :
+           c == 'G' ? 5u  :
+           c == 'H' ? 6u  :
+           c == 'I' ? 7u  :
+           c == 'K' ? 8u  :
+           c == 'L' ? 9u  :
+           c == 'M' ? 10u :
+           c == 'N' ? 11u :
+           c == 'P' ? 12u :
+           c == 'Q' ? 13u :
+           c == 'R' ? 14u :
+           c == 'S' ? 15u :
+           c == 'T' ? 16u :
+           c == 'V' ? 17u :
+           c == 'W' ? 18u :
+           c == 'Y' ? 19u :
+           c == 'B' ? 20u :
+           c == 'Z' ? 21u :
+           c == 'X' ? 22u :
+                      11u;
+}
+
 // convert a given symbol to its ASCII character
 //
 template <Alphabet ALPHABET>
@@ -43,8 +103,8 @@ NVBIO_FORCEINLINE NVBIO_HOST_DEVICE char to_char(const uint8 c)
         return dna_to_char( c );
     else if (ALPHABET == IUPAC16)
         return iupac16_to_char( c );
-    //else if (ALPHABET == PROTEIN) // TODO!
-    //    return protein_to_char( c );
+    else if (ALPHABET == PROTEIN) // TODO!
+        return protein_to_char( c );
 }
 
 // convert a given symbol to its ASCII character
@@ -58,8 +118,8 @@ NVBIO_FORCEINLINE NVBIO_HOST_DEVICE uint8 from_char(const char c)
         return char_to_dna( c );
     else if (ALPHABET == IUPAC16)
         return char_to_iupac16( c );
-    //else if (ALPHABET == PROTEIN) // TODO!
-    //    return char_to_protein( c );
+    else if (ALPHABET == PROTEIN) // TODO!
+        return char_to_protein( c );
 }
 
 // convert from the given alphabet to an ASCII string

85249bff49f38fa678590ad1651340e20d9701e6
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-06-02 08:34:41 -0800

    added missing function
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18541199]

--- nvbio/basic/omp.h
+++ nvbio/basic/omp.h
@@ -37,5 +37,6 @@ inline void omp_set_num_threads(const int n) {}
 inline int  omp_get_max_threads() { return 1; }
 inline int  omp_get_num_threads() { return 1; }
 inline int  omp_get_thread_num()  { return 0; }
+inline int  omp_get_num_procs()   { return 1; }
 
 #endif

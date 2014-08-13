221825527fa03cd73fc5e941fa92ae973a3d0839
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-08-13 06:54:21 -0800

    added not_equal_to_functor
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18781296]

--- nvbio/basic/numbers.h
+++ nvbio/basic/numbers.h
@@ -1087,6 +1087,23 @@ struct equal_to_functor
     const T m_k;
 };
 
+/// A unary functor returning true if op != K
+///
+template <typename T>
+struct not_equal_to_functor
+{
+    typedef T    argument_type;
+    typedef bool result_type;
+
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    not_equal_to_functor(const T k) : m_k( k ) {}
+
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    result_type operator() (const T op) const { return op != m_k; }
+
+    const T m_k;
+};
+
 /// A binary functor returning true if op1 == op2
 ///
 template <typename T>

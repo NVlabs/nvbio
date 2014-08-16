185215a422ccd4f9c2c99c06bfb7606258b612bd
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-08-16 00:25:29 -0800

    removed -Wunused-local_typedefs added platform detection
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18790542]

--- cmake-local/gcc.cmake
+++ cmake-local/gcc.cmake
@@ -2,8 +2,12 @@ if (CMAKE_COMPILER_IS_GNUCC)
     # common GCC compiler flags
     # we add -Wno-unknown-pragmas because of nvcc's #pragma unroll
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wno-unknown-pragmas -Wstrict-aliasing=0")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-unknown-pragmas -Wstrict-aliasing=0")
-    set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG -msse4.2")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-unknown-pragmas -Wstrict-aliasing=0 -Wunused-local-typedefs=0")
+    
+    set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG")
+    if (${CMAKE_SYSTEM_PROCESSOR} MATCHES "Intel")
+        set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -msse4.2 -mpopcnt")
+    endif()
 
     if(WERROR)
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror")

30e04dfe46776710a0bec1d53a19c52d992ca5e1
Jacopo Pantaleoni <jpantaleoni@nvidia.com>
2014-08-21 00:48:43 -0800

    explicitly support
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 18803711]

--- nvbio/basic/primitives.h
+++ nvbio/basic/primitives.h
@@ -32,6 +32,7 @@
 #include <nvbio/basic/console.h>
 #include <nvbio/basic/vector.h>
 #include <nvbio/basic/algorithms.h>
+#include <nvbio/basic/cuda/arch.h>
 #include <thrust/reduce.h>
 #include <thrust/scan.h>
 #include <thrust/copy.h>

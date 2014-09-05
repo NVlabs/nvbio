dd2417d9e3e05db8d7ea0529cf3e2c0487925d91
Nuno Subtil <nsubtil@nvidia.com>
2014-09-05 01:36:19 -0700

    added an extended cuda::load_pointer accepting various load modifiers
    [git-p4: depot-paths = "//sw/gpgpu/nvbio/main/": change = 17915149]

--- nvbio/basic/cuda/ldg.h
+++ nvbio/basic/cuda/ldg.h
@@ -27,6 +27,10 @@
 
 #pragma once
 
+#if defined(__CUDACC__)
+#include <cub/cub.cuh>
+#endif
+
 #include <nvbio/basic/types.h>
 #include <nvbio/basic/iterator.h>
 
@@ -34,8 +38,7 @@ namespace nvbio {
 namespace cuda {
 
 ///
-/// Wrapper class to create a transform iterator out of another base iterator
-/// and a function
+/// Wrapper class to create an __ldg() iterator out of a raw pointer
 ///
 template <typename T>
 struct ldg_pointer
@@ -185,6 +188,185 @@ NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
 ldg_pointer<T> make_ldg_pointer(const T* it)
 {
     return ldg_pointer<T>( it );
+}
+
+
+/**
+ * \brief Enumeration of cache modifiers for memory load operations.
+ */
+enum CacheLoadModifier
+{
+    LOAD_DEFAULT,       ///< Default (no modifier)
+    LOAD_CA,            ///< Cache at all levels
+    LOAD_CG,            ///< Cache at global level
+    LOAD_CS,            ///< Cache streaming (likely to be accessed once)
+    LOAD_CV,            ///< Cache as volatile (including cached system lines)
+    LOAD_LDG,           ///< Cache as texture
+    LOAD_VOLATILE,      ///< Volatile (any memory space)
+};
+
+#if defined(__CUDACC__)
+template <CacheLoadModifier MOD> struct cub_load_mod {};
+
+template <> struct cub_load_mod<LOAD_DEFAULT> { static const cub::CacheLoadModifier MOD = cub::LOAD_DEFAULT; };
+template <> struct cub_load_mod<LOAD_CA>      { static const cub::CacheLoadModifier MOD = cub::LOAD_CA;      };
+template <> struct cub_load_mod<LOAD_CG>      { static const cub::CacheLoadModifier MOD = cub::LOAD_CG;      };
+template <> struct cub_load_mod<LOAD_CS>      { static const cub::CacheLoadModifier MOD = cub::LOAD_CS;      };
+template <> struct cub_load_mod<LOAD_CV>      { static const cub::CacheLoadModifier MOD = cub::LOAD_CV;      };
+template <> struct cub_load_mod<LOAD_LDG>     { static const cub::CacheLoadModifier MOD = cub::LOAD_LDG;     };
+#endif
+
+///
+/// Wrapper class to create a cub::ThreadLoad iterator out of a raw pointer
+///
+template <typename T, CacheLoadModifier MOD>
+struct load_pointer
+{
+    typedef T                                                           value_type;
+    typedef value_type                                                  reference;
+    typedef value_type                                                  const_reference;
+    typedef value_type*                                                 pointer;
+    typedef typename std::iterator_traits<const T*>::difference_type    difference_type;
+    typedef std::random_access_iterator_tag                             iterator_category;
+
+    /// constructor
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer() {}
+
+    /// constructor
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer(const T* base) : m_base( base ) {}
+
+    /// copy constructor
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer(const load_pointer& it) : m_base( it.m_base ) {}
+
+    /// const indexing operator
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    value_type operator[](const uint32 i) const
+    {
+      #if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 350
+        return cub::ThreadLoad<cub_load_mod<MOD>::MOD>( const_cast<T*>(m_base + i) );
+      #else
+        return m_base[i];
+      #endif
+    }
+
+    /// dereference operator
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    value_type operator*() const
+    {
+      #if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 350
+        return cub::ThreadLoad<cub_load_mod<MOD>::MOD>( const_cast<T*>(m_base) );
+      #else
+        return *m_base;
+      #endif
+    }
+
+    /// pre-increment
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer<T,MOD>& operator++()
+    {
+        ++m_base;
+        return *this;
+    }
+
+    /// post-increment
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer<T,MOD> operator++(int i)
+    {
+        load_pointer<T,MOD> r( m_base );
+        ++m_base;
+        return r;
+    }
+
+    /// pre-decrement
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer<T,MOD>& operator--()
+    {
+        --m_base;
+        return *this;
+    }
+
+    /// post-decrement
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer<T,MOD> operator--(int i)
+    {
+        load_pointer<T,MOD> r( m_base );
+        --m_base;
+        return r;
+    }
+
+    /// addition
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer<T,MOD> operator+(const difference_type i) const
+    {
+        return load_pointer( m_base + i );
+    }
+
+    /// subtraction
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer<T,MOD> operator-(const difference_type i) const
+    {
+        return load_pointer( m_base - i );
+    }
+
+    /// addition
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer<T,MOD>& operator+=(const difference_type i)
+    {
+        m_base += i;
+        return *this;
+    }
+
+    /// subtraction
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer<T,MOD>& operator-=(const difference_type i)
+    {
+        m_base -= i;
+        return *this;
+    }
+
+    /// iterator subtraction
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    difference_type operator-(const load_pointer<T,MOD> it) const
+    {
+        return m_base - it.m_base;
+    }
+
+    /// assignment
+    ///
+    NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+    load_pointer& operator=(const load_pointer<T,MOD>& it)
+    {
+        m_base = it.m_base;
+        return *this;
+    }
+
+    const T* m_base;
+};
+
+/// make a load_pointer
+///
+template <CacheLoadModifier MOD, typename T>
+NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
+load_pointer<T,MOD> make_load_pointer(const T* it)
+{
+    return load_pointer<T,MOD>( it );
 }
 
 } // namespace cuda

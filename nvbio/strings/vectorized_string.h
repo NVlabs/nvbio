ae309b57908d2d0d2f8091df331f680beb2c9df7
Jacopo Pantaleoni <jpantaleoni@gmail.com>
2015-02-12 12:53:59 +0100

    more vectorized load fixes

--- nvbio/strings/vectorized_string.h
+++ nvbio/strings/vectorized_string.h
@@ -158,12 +158,10 @@ struct vectorized_string< vector_view<const T*,IndexType> >
     NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
     static uint2 range(const string_type& string)
     {
-        const uint64 alignment = VECTOR_WIDTH_T;
+        const uint64 offset = (uint64(string.base()) / sizeof(T)) % VECTOR_WIDTH_T;
 
-        const uint64 offset = (uint64(string.base()) / sizeof(T)) % alignment;
-
-        const uint64 aligned_begin = offset ? alignment - offset : 0u;
-        const uint64 aligned_end   = aligned_begin + util::round_z( string.size() - aligned_begin, VECTOR_WIDTH );
+        const uint64 aligned_begin = util::round_i( offset,                 VECTOR_WIDTH ) - offset;
+        const uint64 aligned_end   = util::round_z( offset + string.size(), VECTOR_WIDTH ) - offset;
 
         return aligned_begin < string.size() ?
             make_uint2( aligned_begin, aligned_end ) :
@@ -226,8 +224,6 @@ struct vectorized_string< vector_view< PackedStream<InputStream,Symbol,SYMBOL_SI
     NVBIO_FORCEINLINE NVBIO_HOST_DEVICE
     static uint2 range(const string_type& string)
     {
-        const index_type alignment = VECTOR_WIDTH;
-
         const index_type aligned_begin = util::round_i( string.base().index(),                 VECTOR_WIDTH ) - string.base().index();
         const index_type aligned_end   = util::round_z( string.base().index() + string.size(), VECTOR_WIDTH ) - string.base().index();
 
